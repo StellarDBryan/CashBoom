ccb567141dd1d19863a4cb50865d2b0eafeee2db
Bryan Sigala <bryansigala@hotmail.com>
2025-04-26 02:44:25 -0600

    Fix: fix NavLinkBtn by adding <as> attributte to make it Link component

--- app/components/ui/buttons.tsx
+++ app/components/ui/buttons.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@heroui/react";
+import Link from "next/link";
 import React from "react";
 
 
-export function NavLinkBtn({ children, isSelected }: { children: React.ReactNode, isSelected: boolean}) {
+export function NavLinkBtn({ children, isSelected, redirectTo }: { children: React.ReactNode, isSelected: boolean, redirectTo: string}) {
 
     return(
         <>
             <Button
+                as={Link} 
+                href={redirectTo}
                 radius="none"
                 className={`h-full flex flex-col md:flex-row gap-1 items-center justify-center m-0 font-semibold text-[1.4rem] text-inherit bg-transparent border-b-3 border-b-green-700/0 ${isSelected ? 'text-green-700 border-b-green-700/100' : ''} hover:text-green-700 hover:border-b-green-700/100 transition-all ease-in-out duration-200`}
             >
