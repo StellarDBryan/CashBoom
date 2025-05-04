588d5bbf27da586ccff8148d5ce148163eb36552
Bryan Sigala <bryansigala@hotmail.com>
2025-05-04 01:24:11 -0600

    Add: add template to Questionnaires dinamic pages with Step-form and validation

--- app/api/data.tsx
+++ app/api/data.tsx
@@ -1,4 +1,4 @@
-interface question {
+export interface Question {
     id: number, 
     question: string, 
     description: string,
@@ -12,7 +12,7 @@ export interface Questionnaire {
     title: string,
     intro: string,
     icon?: React.ReactNode,
-    questions: question[]
+    questions: Question[]
 }
 
 export interface Challenge {
@@ -112,146 +112,114 @@ export const questionnaires: Questionnaire[] = [
                 "Pedir préstamos para comprar cosas que no podemos pagar. "
             ], 
             answer: 1
-        }
-    ], 
-    icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="text-gray-50 bg-green-700 p-2 rounded-full w-[40px] h-[40px] md:w-[50px] md:h-[50px]">
-          <g fill="none">
-            <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m11.287 4.717l.307-.13c2.85-1.268 5.68-1.237 7.531-.996l.41.058l.372.063l.333.065l.29.063c.864.198 1.404.937 1.464 1.731l.006.15v11.585c0 1.027-.976 1.701-1.893 1.49l-.28-.062l-.325-.062l-.365-.06a14 14 0 0 0-.616-.079l-.453-.04c-1.483-.104-3.414.001-5.355.79l-.307.13c-2.63 1.17-5.244 1.234-7.087 1.047l-.444-.05l-.41-.06l-.372-.063l-.172-.032l-.45-.095c-.865-.198-1.405-.937-1.465-1.731L2 18.279V6.694c0-1.027.976-1.702 1.893-1.49l.28.062l.325.062l.365.06c1.536.23 3.972.326 6.424-.671M12 8a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4"/>
-          </g> 
-        </svg>
-    )
-  }, 
-  {
-    id: 1, 
-    levelId: 1,
-    title: "Conceptos Básicos de Finanzas", 
-    intro: "Aprende los fundamentos financieros escenciales. ", 
-    questions: [
+        }, 
         {
-            id: 1, 
-            question: "¿Qué es el ahorro?", 
-            description: "El ahorro es una parte fundamental de las finanzas personales. Selecciona la definición correcta: ", 
-            options: [
-                "Gastar todo el dinero que se gana inmediatamente. ", 
-                "Guardar dinero para gastos futuros, metas o emergencias. ", 
-                "Invertir todo el dinero en activos de alto riesgo. ", 
-                "Pedir préstamos para comprar cosas que no podemos pagar. "
-            ], 
-            answer: 1
-        }
-    ], 
-    icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="text-gray-50 bg-green-700 p-2 rounded-full w-[40px] h-[40px] md:w-[50px] md:h-[50px]">
-          <g fill="none">
-            <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m11.287 4.717l.307-.13c2.85-1.268 5.68-1.237 7.531-.996l.41.058l.372.063l.333.065l.29.063c.864.198 1.404.937 1.464 1.731l.006.15v11.585c0 1.027-.976 1.701-1.893 1.49l-.28-.062l-.325-.062l-.365-.06a14 14 0 0 0-.616-.079l-.453-.04c-1.483-.104-3.414.001-5.355.79l-.307.13c-2.63 1.17-5.244 1.234-7.087 1.047l-.444-.05l-.41-.06l-.372-.063l-.172-.032l-.45-.095c-.865-.198-1.405-.937-1.465-1.731L2 18.279V6.694c0-1.027.976-1.702 1.893-1.49l.28.062l.325.062l.365.06c1.536.23 3.972.326 6.424-.671M12 8a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4"/>
-          </g> 
-        </svg>
-    )
-  }, 
-  {
-    id: 1, 
-    levelId: 1,
-    title: "Conceptos Básicos de Finanzas", 
-    intro: "Aprende los fundamentos financieros escenciales. ", 
-    questions: [
+          id: 2, 
+          question: "¿Cuál es la diferencia entre ingresos y gastos?", 
+          description: "Es importante entender estos conceptos básicos para poder gestionar tus finanzas. Selecciona la respuesta correcta: ", 
+          options: [
+            "Los ingresos son gastos pequeños y los gastos con ingresos grandes. ", 
+            "Los ingresos y gastos son lo mismo, solo cambia el momento en que ocurren.", 
+            "Los ingresos son el dinero que entra a tus cuentas y los gastos son el dinero que sale. ", 
+            "Los ingresos solo son el sueldo y los gastos solo son las compras en tiendas. "
+          ], 
+          answer: 2
+        }, 
         {
-            id: 1, 
-            question: "¿Qué es el ahorro?", 
-            description: "El ahorro es una parte fundamental de las finanzas personales. Selecciona la definición correcta: ", 
-            options: [
-                "Gastar todo el dinero que se gana inmediatamente. ", 
-                "Guardar dinero para gastos futuros, metas o emergencias. ", 
-                "Invertir todo el dinero en activos de alto riesgo. ", 
-                "Pedir préstamos para comprar cosas que no podemos pagar. "
-            ], 
-            answer: 1
-        }
-    ], 
-    icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="text-gray-50 bg-green-700 p-2 rounded-full w-[40px] h-[40px] md:w-[50px] md:h-[50px]">
-          <g fill="none">
-            <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m11.287 4.717l.307-.13c2.85-1.268 5.68-1.237 7.531-.996l.41.058l.372.063l.333.065l.29.063c.864.198 1.404.937 1.464 1.731l.006.15v11.585c0 1.027-.976 1.701-1.893 1.49l-.28-.062l-.325-.062l-.365-.06a14 14 0 0 0-.616-.079l-.453-.04c-1.483-.104-3.414.001-5.355.79l-.307.13c-2.63 1.17-5.244 1.234-7.087 1.047l-.444-.05l-.41-.06l-.372-.063l-.172-.032l-.45-.095c-.865-.198-1.405-.937-1.465-1.731L2 18.279V6.694c0-1.027.976-1.702 1.893-1.49l.28.062l.325.062l.365.06c1.536.23 3.972.326 6.424-.671M12 8a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4"/>
-          </g> 
-        </svg>
-    )
-  }, 
-  {
-    id: 1, 
-    levelId: 1,
-    title: "Conceptos Básicos de Finanzas", 
-    intro: "Aprende los fundamentos financieros escenciales. ", 
-    questions: [
+          id: 3, 
+          question: "¿Qué es un presupuesto?", 
+          description: "Un presupuesto es una herramienta fundamental para la salud financiera. Selecciona la definición más adecuada: ", 
+          options: [
+            "Un documento que solo usan las grandes empresas. ", 
+            "Un plan financiero que estima ingresos y gastos durante un período determinado.", 
+            "Una lista de todas las deudas que tienes actualmente. ", 
+            "Una estrategia para invertir en bolsa de valores. "
+          ], 
+          answer: 1
+        }, 
         {
-            id: 1, 
-            question: "¿Qué es el ahorro?", 
-            description: "El ahorro es una parte fundamental de las finanzas personales. Selecciona la definición correcta: ", 
-            options: [
-                "Gastar todo el dinero que se gana inmediatamente. ", 
-                "Guardar dinero para gastos futuros, metas o emergencias. ", 
-                "Invertir todo el dinero en activos de alto riesgo. ", 
-                "Pedir préstamos para comprar cosas que no podemos pagar. "
-            ], 
-            answer: 1
-        }
-    ], 
-    icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="text-gray-50 bg-green-700 p-2 rounded-full w-[40px] h-[40px] md:w-[50px] md:h-[50px]">
-          <g fill="none">
-            <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m11.287 4.717l.307-.13c2.85-1.268 5.68-1.237 7.531-.996l.41.058l.372.063l.333.065l.29.063c.864.198 1.404.937 1.464 1.731l.006.15v11.585c0 1.027-.976 1.701-1.893 1.49l-.28-.062l-.325-.062l-.365-.06a14 14 0 0 0-.616-.079l-.453-.04c-1.483-.104-3.414.001-5.355.79l-.307.13c-2.63 1.17-5.244 1.234-7.087 1.047l-.444-.05l-.41-.06l-.372-.063l-.172-.032l-.45-.095c-.865-.198-1.405-.937-1.465-1.731L2 18.279V6.694c0-1.027.976-1.702 1.893-1.49l.28.062l.325.062l.365.06c1.536.23 3.972.326 6.424-.671M12 8a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4"/>
-          </g> 
-        </svg>
-    )
-  }, 
-  {
-    id: 1, 
-    levelId: 1,
-    title: "Conceptos Básicos de Finanzas", 
-    intro: "Aprende los fundamentos financieros escenciales. ", 
-    questions: [
+          id: 4, 
+          question: "¿Qué es la tasa de interés?", 
+          description: "La tasa de interés afecta tus préstamos y ahorros. Selecciona la respuesta correcta: ", 
+          options: [
+            "La cantidad de dinero que debes pagar a tu familia cuando te prestan dinero. ", 
+            "Un impuesto que solo pagan las personas con altos ingresos. ", 
+            "El porcentaje que se cobra o paga por el uso del dinero durante un tiempo específico. ", 
+            "El precio de compra venta de acciones en la bolsa. "
+          ], 
+          answer: 2
+        }, 
         {
-            id: 1, 
-            question: "¿Qué es el ahorro?", 
-            description: "El ahorro es una parte fundamental de las finanzas personales. Selecciona la definición correcta: ", 
-            options: [
-                "Gastar todo el dinero que se gana inmediatamente. ", 
-                "Guardar dinero para gastos futuros, metas o emergencias. ", 
-                "Invertir todo el dinero en activos de alto riesgo. ", 
-                "Pedir préstamos para comprar cosas que no podemos pagar. "
-            ], 
-            answer: 1
-        }
-    ], 
-    icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="text-gray-50 bg-green-700 p-2 rounded-full w-[40px] h-[40px] md:w-[50px] md:h-[50px]">
-          <g fill="none">
-            <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
-            <path fill="currentColor" d="m11.287 4.717l.307-.13c2.85-1.268 5.68-1.237 7.531-.996l.41.058l.372.063l.333.065l.29.063c.864.198 1.404.937 1.464 1.731l.006.15v11.585c0 1.027-.976 1.701-1.893 1.49l-.28-.062l-.325-.062l-.365-.06a14 14 0 0 0-.616-.079l-.453-.04c-1.483-.104-3.414.001-5.355.79l-.307.13c-2.63 1.17-5.244 1.234-7.087 1.047l-.444-.05l-.41-.06l-.372-.063l-.172-.032l-.45-.095c-.865-.198-1.405-.937-1.465-1.731L2 18.279V6.694c0-1.027.976-1.702 1.893-1.49l.28.062l.325.062l.365.06c1.536.23 3.972.326 6.424-.671M12 8a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4"/>
-          </g> 
-        </svg>
-    )
-  }, 
-  {
-    id: 1, 
-    levelId: 1,
-    title: "Conceptos Básicos de Finanzas", 
-    intro: "Aprende los fundamentos financieros escenciales. ", 
-    questions: [
+          id: 5, 
+          question: "¿Qué es la liquidez?", 
+          description: "La liquidez es un concepto financiero importante. ¿Cuál es su significado? ", 
+          options: [
+            "La cantidad de dinero físico que (billetes y monedas) que tienes en efectivo. ", 
+            "La facilidad con la que un activo puede convertirse en dinero sin perder valor. ", 
+            "La cantidad de dinero que tienes invertido en bienes inmuebles. ", 
+            "El valor total de tus deudas pendientes actualmente. "
+          ], 
+          answer: 1
+        }, 
         {
-            id: 1, 
-            question: "¿Qué es el ahorro?", 
-            description: "El ahorro es una parte fundamental de las finanzas personales. Selecciona la definición correcta: ", 
-            options: [
-                "Gastar todo el dinero que se gana inmediatamente. ", 
-                "Guardar dinero para gastos futuros, metas o emergencias. ", 
-                "Invertir todo el dinero en activos de alto riesgo. ", 
-                "Pedir préstamos para comprar cosas que no podemos pagar. "
-            ], 
-            answer: 1
+          id: 6, 
+          question: "¿Qué es la inflación?", 
+          description: "La inflación afecta nuestro poder adquisitivo. Selecciona la definición correcta: ", 
+          options: [
+            "El aumento general de precios y la disminución del poder adquisitivo del dinero. ", 
+            "El aumento en el valor de las propiedades inmobiliarias únicamente. ", 
+            "La disminución de las tasas de interés bancarias. ", 
+            "El aumento de los salarios en un país. "
+          ], 
+          answer: 0
+        }, 
+        {
+          id: 7, 
+          question: "¿Qué es el patrimonio neto?", 
+          description: "El patrimonio neto es una métrica importante para evaluar tu situación financiera. ¿Cómo se calcula? ", 
+          options: [
+            "Sumando todos tus ingresos mensuales. ", 
+            "Restando el valor de tus pasivos (deudas) al valor de tus activos (lo que posees). ", 
+            "Multiplicando tus ingresos por 12 meses. ", 
+            "Sumando el valor de todas tus propiedades inmobiliarias. "
+          ], 
+          answer: 1
+        }, 
+        {
+          id: 8, 
+          question: "¿Qué es una tarjeta de crédito?", 
+          description: "Las tarjetas de crédito son instrumentos financieros comunes. ¿Cuál es su característica principal? ", 
+          options: [
+            "Una tarjeta que te permite gastar todo el dinero que tienes en tu cuenta bancaria. ", 
+            "Una tarjeta que te permite utilizar dinero prestado por el banco con un límite establecido. ", 
+            "Una tarjeta que solo se puede usar para compras en tiendas específicas. ", 
+            "Una tarjeta que te da acceso a todas tus cuentas de inversión. "
+          ], 
+          answer: 1
+        }, 
+        {
+          id: 9, 
+          question: "¿Qué es la diversificación en finanzas?", 
+          description: "La diversificación es un principio importante para la inversión. ¿Qué significa?", 
+          options: [  
+            "Tener múltiples cuentas bancarias en diferentes países para evitar impuestos. ", 
+            "Distribuir el dinero en diferentes tipos de inversiones para reducir el riesgo. ", 
+            "Cambiar frecuentemente de trabajo para aumentar los ingresos. ", 
+            "Invertir todo tu dinero en un solo negocio prometedor. "
+          ], 
+          answer: 1
+        }, 
+        {
+          id: 10, 
+          question: "¿Qué es la educación financiera?", 
+          description: "La educación financiera es clave para el bienestar económico. ¿Cuál es su definición más adecuada?", 
+          options: [
+            "Estudiar economía en la universidad. ", 
+            "El proceso de aprender cómo administrar el dinero y tomar decisiones financieras informadas. ", 
+            "Conocer los nombres de todos los bancos de tu país. ", 
+            "Memorizar las tasas de interés de todos los productos financieros disponibles. "
+          ], 
+          answer: 1
         }
     ], 
     icon: (
--- app/questionnaire/[slug]/page.tsx
+++ app/questionnaire/[slug]/page.tsx
@@ -1,13 +1,129 @@
+"use client";
+
 import { questionnaires } from "@/app/api/data"
+import { Card, CardHeader, CardBody, CardFooter, Radio, RadioGroup, cn, Button, Pagination } from "@heroui/react";
+import { useState } from "react";
+import { Jersey_15 } from 'next/font/google'; 
 
-export default function Page({ params }: { params: { slug: number } }) {
+const jersey = Jersey_15({
+    subsets: ["latin"], 
+    weight: '400'
+});
 
-    const questionnaire = questionnaires.find(q => q.id === params.slug);
+export default function Page({ params }: { params: { slug: string } }) {
+
+    const [ step, setStep ] = useState(0);
+    const [ answers, setAnswers ] = useState<number[]>([]);
+    const [ questionsAnswered, setQuestionsAnswered ] = useState<number[]>([]);
+    const [ showCorrectAnswer, setShowCorrectAnswer ] = useState(false);
+    const [ selectedAnswer, setSelectedAnswer ] = useState<number | null>(null);
+
+    const questionnaire = questionnaires.find(q => q.id === Number(params.slug));
+    const currentQuestion = questionnaire?.questions[step];
+
+    const handleCheckAnswer = () => {
+        if (selectedAnswer !== null) {
+            setShowCorrectAnswer(true);
+            setAnswers(prev => [...prev, selectedAnswer]);
+            setQuestionsAnswered(prev => [...prev, step]);
+        }
+    };
+
+    const handlePageChange = (newStep: number) => {
+        if (!questionsAnswered.includes(step) && selectedAnswer !== null) {
+            setAnswers(prev => [...prev, selectedAnswer]);
+            setQuestionsAnswered(prev => [...prev, step]);
+        }
+        setStep(newStep);
+        setShowCorrectAnswer(false);
+        setSelectedAnswer(null);
+    };
+
+    const isQuestionAnswered = questionsAnswered.includes(step);
+    const currentAnswer = answers[questionsAnswered.indexOf(step)];
 
     return(
-        <main>
-            <div>
+        <main className="w-full bg-gray-50 flex flex-col gap-2 py-5 px-2 md:px-8 overflow-hidden relative">
+            <div className="object-cover absolute right-[16%] opacity-30 bottom-[20%] transform scale-[9] -rotate-12 pointer-events-none">
+                {questionnaire?.icon}
+            </div>
+            <h2 className={`${jersey.className} md:ml-[10%] lg:ml-[16.6666665%] text-[2rem] sm:text-[2.5rem] md:text-[3.2rem] font-semibold text-green-700`}>
                 {questionnaire?.title}
+            </h2>
+            <div className="flex flex-col gap-3 w-[95%] sm:w-[90%] md:w-4/5 lg:w-2/3 mx-auto">
+                <p className="text-h6 text-neutral-700 font-medium text-wrap">
+                    {questionnaire?.intro}
+                </p>
+                <Card className="w-full p-5 rounded-lg bg-inherit shadow-none">
+                    <CardHeader className="text-[1.25rem] md:text-[1.5rem] font-semibold">
+                        {currentQuestion?.question}
+                    </CardHeader>
+                    <CardBody className="flex flex-col gap-5">
+                        <p className="text-[1.1rem] md:text-[1.2rem] font-medium">
+                            {currentQuestion?.description}
+                        </p>
+                        <div>
+                            <RadioGroup 
+                                isReadOnly={isQuestionAnswered || showCorrectAnswer}
+                                value={isQuestionAnswered ? currentQuestion?.options[currentAnswer] : undefined}
+                                onValueChange={(value) => {
+                                    setSelectedAnswer(currentQuestion?.options.findIndex(o => o === value) ?? null);
+                                    setShowCorrectAnswer(false);
+                                }}
+                                classNames={{
+                                    wrapper: 'flex flex-col gap-5'
+                                }}
+                            >
+                                {currentQuestion?.options.map((option, id) => (
+                                    <Radio key={`${currentQuestion.id}-${id}`} value={option}
+                                        classNames={{
+                                            base: cn(
+                                                'flex flex-row-reverse justify-between gap-3 font-medium rounded-lg px-2 py-4 text-wrap border-2 boder-black', 
+                                                !showCorrectAnswer && 'data-[selected=true]:border-success data-[selected=true]:bg-green-600/10',
+                                                showCorrectAnswer && id === currentQuestion?.answer && 'border-success bg-green-600/10 animate-pulse',
+                                                showCorrectAnswer && id === selectedAnswer && id !== currentQuestion?.answer && 'border-danger bg-red-600/30 animate-pulse'
+                                            ),
+                                            label: 'text-[1.1rem]', 
+                                            wrapper: 'bg-gray-100'
+                                        }}
+                                    >
+                                        {option}
+                                    </Radio>
+                                ))}
+                            </RadioGroup>
+                        </div>
+                    </CardBody>
+                    <CardFooter>
+                        {!showCorrectAnswer && !isQuestionAnswered && 
+                        <Button 
+                            className="flex flex-row items-center gap-2 py-2 px-6 rounded-lg bg-green-600 text-[1.1rem] font-semibold text-gray-50"
+                            onPress={handleCheckAnswer}
+                            isDisabled={selectedAnswer === null || isQuestionAnswered}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M18 5v6h-1v1h-2v1h-1v2H9v-3h1v-1h1v-1h2V9h1V7h-1V6h-2v1h-1v1H9v1H8V8H7V7H6V6H5V5h1V4h1V3h2V2h6v1h1v1h1v1zm-5 13h1v3h-1v1h-3v-1H9v-3h1v-1h3z"/>
+                            </svg>
+                            Comprovar
+                        </Button>}
+                        {showCorrectAnswer && 
+                        <Button 
+                            onPress={() => handlePageChange(step+1)}
+                            className="flex flex-row flex-nowrap items-center group gap-2 px-6 py-4 text-[1.1rem] font-semibold text-neutral-700 bg-gray-200 rounded-lg">
+                            Continuar
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" className="transform group-hover:translate-x-1 transition-transform ease-in-out duration-200">
+                                <path fill="none" stroke="currentColor" strokeWidth="2" d="M2 12h20m-9-9l9 9l-9 9"/>
+                            </svg>
+                        </Button>}
+                    </CardFooter>
+                </Card>
+                <Pagination 
+                    className="mx-auto"
+                    initialPage={1} 
+                    page={step + 1}
+                    total={questionnaire!.questions.length}
+                    color="success"
+                    onChange={(page) => handlePageChange(page - 1)}
+                />
             </div>
         </main>
     );
