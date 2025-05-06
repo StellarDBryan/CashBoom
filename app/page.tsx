288077ddeae4fdc49567e47b42cb3c197a5ed7e0
Bryan Sigala <bryansigala@hotmail.com>
2025-05-05 23:10:51 -0600

    Add: add app-template image to show exaple of app in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div className="">
-      Home page
-    </div>
+    <main className="min-h-screen w-full bg-gray-50">
+      <Image
+        src='/images/common/app-template.png'
+        alt="App Showcase"
+        width={300}
+        height={600}
+      />
+    </main>
   );
 }
