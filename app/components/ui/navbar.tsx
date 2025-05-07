5875d824c07619bc03e592bc13144e69186981ee
Bryan Sigala <bryansigala@hotmail.com>
2025-05-06 23:58:49 -0600

    Style: fix Login btn in navbar while there's no session

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -76,7 +76,12 @@ export default function Navbar(){
                         </>
                     : 
                         pathname !== "/login" 
-                            ? <Button as={Link} href="/login" radius="sm">
+                            ? <Button as={Link} href="/login" radius="sm"
+                                className="flex flex-row flex-nowrap items-center gap-2 mr-3 md:mr-0 px-2 sm:px-6 py-2 border-b-2 border-transparent hover:border-green-600 rounded-none text-[1.1rem] sm:text-[1.2rem] bg-inherit text-green-600 font-semibold"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                    <path fill="currentColor" fillRule="evenodd" d="M11 2a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zm1.293 6.293a1 1 0 0 1 1.414 0l3 3a1 1 0 0 1 0 1.414l-3 3a1 1 0 0 1-1.414-1.414L13.586 13H5a1 1 0 1 1 0-2h8.586l-1.293-1.293a1 1 0 0 1 0-1.414" clipRule="evenodd"/>
+                                </svg>
                                 Iniciar sesión 
                             </Button> 
                             : ''}
